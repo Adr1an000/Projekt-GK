1d31a1b62a69c36f8e76589839d436570ea8db72
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-03-07 14:52:06 +0100

    Enemy system - testy
    Jakieś testowanie zachowania się przeciwnika. Na razie idzie prosto, a jak napotka ściane to zawraca.

---
+++ ProjektGK/Assets/GameSettings.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    [SerializeField]
+    public float EnemySpeed = 2f;
+
+ //   public static float EnemySpeed => Instance.enemySpeed;
+
+    [SerializeField]
+    public float AggroRadius = 4f;
+
+ //   public static float AggroRadius => Instance.aggroRadius;
+
+    [SerializeField]
+    public float AttackRange = 3f;
+
+ //   public static float AttackRange => Instance.attackRange;
+
+    /*
+    [SerializeField] private GameObject enemyProjectilePrefab;
+    public static GameObject EnemyProjectilePrefab => Instance.enemyProjectilePrefab;
+    */
+
+    public static GameSettings Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance!= null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
