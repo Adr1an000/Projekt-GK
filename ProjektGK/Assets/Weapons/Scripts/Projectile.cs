514bca0073ed23e64c7cf3a86f757db3de2d6979
Adr1an000 <32631648+Adr1an000@users.noreply.github.com>
2020-03-07 15:58:10 +0100

    Basic weapons and projectiles

---
+++ ProjektGK/Assets/Weapons/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float elapsedTime = 0f;
+
+    public Vector3 Velocity { get; set; }
+    public float Lifetime { get; set; }
+    public float Damage { get; set; }
+    public int Affiliation { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Velocity * Time.deltaTime;
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= Lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
