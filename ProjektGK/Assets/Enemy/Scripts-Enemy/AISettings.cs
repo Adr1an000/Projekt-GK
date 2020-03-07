a28eece3dbb29e6735182815f9ee731680ded7e8
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-03-07 17:37:39 +0100

    Poprawki w kodzie enemy (troche posprzątane)
    Posprzątanie kodu

--- ProjektGK/Assets/Enemy/Scripts-Enemy/AISettings.cs
+++ ProjektGK/Assets/Enemy/Scripts-Enemy/AISettings.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AISettings : MonoBehaviour
+public static class AISettings
 {
-    public float wanderSpeed = 2;
-    public float chaseSpeed = 2;
-    public float turnSpeed = 2;
-    public float attackRange = 2;
-    public float AttackSpeed()
+    [SerializeField]
+    public static float PatrolSpeed { get; set; } = 2.0f;
+
+    [SerializeField]
+    public static float ChaseSpeed { get; set; } = 4.0f;
+
+    [SerializeField]
+    public static float TurnSpeed { get; set; } = 2.0f;
+
+    [SerializeField]
+    public static float AttackRange { get; set; } = 10.0f;
+
+    [SerializeField]
+    public static float DetectionRange { get; set; } = 2.0f;
+
+    [SerializeField]
+    public static LayerMask opponentLayers;
+
+    [SerializeField]
+    public static LayerMask obstacleLayerMask;
+
+    public static float AttackSpeed()
     {
         return Random.Range(-1, 2);
     }
-    public float frenzyLevel = 2;
-    public int detectionRange = 2;
-    public LayerMask opponentLayers;
-    public LayerMask obstacleLayerMask;
 }
