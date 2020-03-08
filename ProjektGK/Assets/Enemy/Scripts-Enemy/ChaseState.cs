320a162b44a11a39fa248ab6d4c7e2cb89e973f2
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-03-08 18:53:23 +0100

    Naprawa bugów enemy
    Przeciwnik jak zauważy gracza to idzie do miejsca w którym ostatnio się znajdował. Po tym, jak dotrze i go nie widzi to przechodzi w stan patrolowania.

--- ProjektGK/Assets/Enemy/Scripts-Enemy/ChaseState.cs
+++ ProjektGK/Assets/Enemy/Scripts-Enemy/ChaseState.cs
@@ -7,25 +7,44 @@ public class ChaseState : EnemyBaseState
 {
     private EnemyAI enemyAI;
 
+    private bool chasePlayer = false;
+
+    private Vector3 targetLastLocation;
+
     public ChaseState(EnemyAI _enemyAI) : base(_enemyAI.gameObject)
     {
         enemyAI = _enemyAI;
+        chasePlayer = true;
     }
 
     public override Type StatePerform()
     {
         if(enemyAI.Target == null)
         {
+            chasePlayer = true;
+
             return typeof(PatrolState);
         }
 
-        Debug.Log("Chasing player");
+        if(chasePlayer == true)
+        {
+            transform.LookAt(enemyAI.Target);
 
-        transform.LookAt(enemyAI.Target);
+            targetLastLocation = enemyAI.Target.position;
 
-        enemyAI.agent.SetDestination(enemyAI.Target.position);
+            chasePlayer = false;
+        }
 
-      
+        enemyAI.agent.SetDestination(targetLastLocation);
+
+        enemyAI.agent.stoppingDistance = 5;
+
+        if (Vector3.Distance(transform.position, enemyAI.agent.destination) <= 5f)
+        {
+            enemyAI.agent.isStopped = true;
+            enemyAI.agent.ResetPath();
+            enemyAI.Target = null;
+        }
 
         return null;
     }
