0c96d7919bd1a25c2d3666eb2097bf30d238db92
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-03-11 15:25:51 +0100

    Enemy Spawner Beta - Portal
    Dodałem spawner enemy. Trzeba przede wszystkim później poprawić wygląd i dodać jakiś limit przeciwników czy coś.

---
+++ ProjektGK/Assets/Enemy/Scripts-Enemy/EnemySpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+
+    public GameObject spawnedEnemy;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", spawnTime, spawnDelay);
+    }
+
+    public void SpawnEnemy()
+    {
+        Instantiate(spawnedEnemy, transform.position, transform.rotation);
+
+        if(stopSpawning)
+        {
+            CancelInvoke("SpawnEnemy");
+        }
+    }
+}
