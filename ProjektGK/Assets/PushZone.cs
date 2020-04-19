5f935bdd6da3f5a2fa982ff03eeab4924c66ff6f
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-04-19 19:10:33 +0200

    Ładunek (beta)
    Dodałęm wózek z bombą poruszający się po danych punktach, kiedy gracz jest blisko.

---
+++ ProjektGK/Assets/PushZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushZone : MonoBehaviour
+{
+    public CartFollowPath cartFollowPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            cartFollowPath.speed = 20;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            cartFollowPath.speed = 0;
+        }
+    }
+}
