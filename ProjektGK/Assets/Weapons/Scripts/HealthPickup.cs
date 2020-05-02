9f8c65148ad057c6fd02ea3299f60aa15b0fdc98
Adr1an000 <32631648+Adr1an000@users.noreply.github.com>
2020-05-02 19:47:09 +0200

    Pickups added
    TODO: add textures

---
+++ ProjektGK/Assets/Weapons/Scripts/HealthPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    public int amount = 50;
+
+    public void PickUp(GameObject player)
+    {
+        var health = player.GetComponent<Health>();
+        if (health)
+        {
+            if (health.currentHealth < health.maxHealth)
+            {
+                health.Heal(amount);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
