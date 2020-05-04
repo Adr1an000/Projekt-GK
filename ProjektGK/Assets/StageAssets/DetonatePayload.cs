63aa3f07b640d87942c96faf45f00a7a8b16bf43
pawel0705 <32631610+pawel0705@users.noreply.github.com>
2020-05-04 21:36:13 +0200

    Eksplozja ładunku + przejście do ekranu zwycięstwa

---
+++ ProjektGK/Assets/StageAssets/DetonatePayload.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetonatePayload : MonoBehaviour
+{
+    public GameObject payloadToExplode;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Payload")
+        {
+            payloadToExplode.GetComponent<Bomb>().explode = true;
+        }
+    }
+}
