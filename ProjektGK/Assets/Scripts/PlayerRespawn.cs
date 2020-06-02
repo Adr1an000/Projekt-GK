0e07cf75f9b3e622af0cedfef71e5c8fda2b133f
Dawid Musialik <dawidmusialik1998@gmail.com>
2020-06-02 23:03:32 +0200

    Details
    Barrels bug fixed.
    Damage after death fixed.
    After respawn player is  invincible.

--- ProjektGK/Assets/Scripts/PlayerRespawn.cs
+++ ProjektGK/Assets/Scripts/PlayerRespawn.cs
@@ -7,10 +7,11 @@ public class PlayerRespawn : MonoBehaviour
     // Start is called before the first frame update
     public Transform respawnPoint = null;
     public int restoreAmmo = 100;
-    public int restoreHealth = 200;
+    public int restoreHealth = 200; 
     private Health playerHealth = null;
     private WeaponManager weaponManager = null;
     public CartFollowPath cartFollowPath;
+    public int invincibileTime = 3;
 
     public void SetRespawnPoint(Transform newPoint)
     {
@@ -28,6 +29,7 @@ public class PlayerRespawn : MonoBehaviour
             playerHealth.Heal(restoreHealth);
             weaponManager.ammo += restoreAmmo;
             cartFollowPath.speed = 0;
+            StartCoroutine(SetPlayerInvincivile());
         }
     }
     private void Start()
@@ -35,5 +37,13 @@ public class PlayerRespawn : MonoBehaviour
         playerHealth = GetComponent<Health>();
         playerHealth.onDeath.AddListener(Respawn);
         weaponManager = GetComponent<WeaponManager>();
+    }
+    //After respawn, sets player invincibile for given time.
+    IEnumerator SetPlayerInvincivile()
+    {
+        //Można tutaj wrzucić wyświetlanie ekranu nieśmiertelności.
+        GetComponent<Health>().invincible = true;
+        yield return new WaitForSeconds (invincibileTime);
+        GetComponent<Health>().invincible = false;
     }
 }
