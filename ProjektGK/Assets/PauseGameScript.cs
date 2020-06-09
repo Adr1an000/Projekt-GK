24e9998bb1df9d6e02d89ebbf520b520f682ff50
Dawid Musialik <dawidmusialik1998@gmail.com>
2020-06-09 19:36:23 +0200

    Podpisanie skryptów

--- ProjektGK/Assets/PauseGameScript.cs
+++ ProjektGK/Assets/PauseGameScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//authors:________, Dawid Musialik
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
--- ProjektGK/Assets/SoundManager.cs
+++ ProjektGK/Assets/SoundManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Authors:______, Dawid Musialik
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
